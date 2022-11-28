137a19a9a4dfb95389b7e944c99530ebaae312a5
Cole <colewestbrookphoto@gmail.com>
2022-11-28 12:04:26 -0500

    First draft browse page and components

--- components/ProjCarousel/ProjCarouselNav.js
+++ components/ProjCarousel/ProjCarouselNav.js
@@ -6,17 +6,19 @@ const DUMMY_PROJECT_DOTS = [ "inactive", "inactive","inactive","active","inactiv
 
 const ProjCarouselNav = props => {
   const ICON_SIZE = 30
-
+  const ICON_COLOR = "gray"
 
   return (
     <div className="flex justify-center items-center my-4">
-      <FaRegArrowAltCircleLeft size={ICON_SIZE}/>
-          {DUMMY_PROJECT_DOTS.map((dot) => {
-            return (
-              dot == "active" ? <FaCircle size={ICON_SIZE} /> : <FaRegCircle size={ICON_SIZE} />
-            );
-          })}
-      <FaRegArrowAltCircleRight size={ICON_SIZE} />
+      <FaRegArrowAltCircleLeft size={ICON_SIZE} color={ICON_COLOR} />
+      {DUMMY_PROJECT_DOTS.map((dot) => {
+        return dot == "active" ? (
+          <FaCircle color={ICON_COLOR} size={ICON_SIZE} />
+        ) : (
+          <FaRegCircle color={ICON_COLOR} size={ICON_SIZE} />
+        );
+      })}
+      <FaRegArrowAltCircleRight color={ICON_COLOR} size={ICON_SIZE} />
     </div>
   );
 };
